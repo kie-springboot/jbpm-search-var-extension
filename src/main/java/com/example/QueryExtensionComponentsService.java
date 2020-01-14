78ce6992e99d980a40c491bc8c98e3adc883cae3
Anton Giertli <you@example.com>
2020-01-14 10:33:26 +0000

    refactoring

--- src/main/java/com/example/QueryExtensionComponentsService.java
+++ src/main/java/com/example/QueryExtensionComponentsService.java
@@ -5,13 +5,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import javax.persistence.EntityManagerFactory;
-
 import org.jbpm.runtime.manager.impl.jpa.EntityManagerFactoryManager;
-import org.jbpm.services.api.RuntimeDataService;
-import org.jbpm.services.api.query.QueryService;
 import org.kie.server.services.api.KieServerApplicationComponentsService;
-import org.kie.server.services.api.KieServerRegistry;
 import org.kie.server.services.api.SupportedTransports;
 import org.kie.server.services.jbpm.JbpmKieServerExtension;
 import org.slf4j.Logger;
@@ -32,26 +27,9 @@ public class QueryExtensionComponentsService implements KieServerApplicationComp
 
 		logger.info("Registering QueryExtensionComponentsService");
 
-		KieServerRegistry kieServerRegistry = null;
-		QueryService queryService = null;
-		RuntimeDataService runtimeDataService = null;
-
-//		for (Object object : services) {
-//			if (RuntimeDataService.class.isAssignableFrom(object.getClass())) {
-//				runtimeDataService = (RuntimeDataService) object;
-//				continue;
-//			}
-//
-//		}
-//			} else if (KieServerRegistry.class.isAssignableFrom(object.getClass())) {
-//				kieServerRegistry = (KieServerRegistry) object;
-//				continue;
-//			}
-//		}
-
 		List<Object> components = new ArrayList<Object>(1);
 		if (SupportedTransports.REST.equals(supportedTransports)) {
-			components.add(new VariablesQuery(EntityManagerFactoryManager.get().getOrCreate(PU_NAME)));
+			components.add(new VariablesQueryResource(EntityManagerFactoryManager.get().getOrCreate(PU_NAME)));
 		}
 		logger.info("QueryExtensionComponentsService registered");
 
---
+++ src/main/java/com/example/VariablesQueryResource.java
@@ -0,0 +1,102 @@
+package com.example;
+
+import static org.kie.server.remote.rest.common.util.RestUtils.createResponse;
+import static org.kie.server.remote.rest.common.util.RestUtils.getContentType;
+import static org.kie.server.remote.rest.common.util.RestUtils.getVariant;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Variant;
+
+import org.kie.server.api.marshalling.Marshaller;
+import org.kie.server.api.marshalling.MarshallerFactory;
+import org.kie.server.api.marshalling.MarshallingFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("server/queries/variables")
+public class VariablesQueryResource {
+
+	private static final Logger logger = LoggerFactory.getLogger(VariablesQueryResource.class);
+	private static Boolean PRINT_VERBOSE = true;
+	private EntityManagerFactory emf;
+	private static final String EMPTY_RESULT = "[ ]";
+	private VariablesQueryRequest searchRequest;
+	private VariablesQueryService searchService;
+	private Marshaller marshaller;
+
+	public VariablesQueryResource(EntityManagerFactory emf) {
+		this.emf = emf;
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(BPMTask.class);
+		this.marshaller = MarshallerFactory.getMarshaller(classes, MarshallingFormat.JSON,
+				VariablesQueryResource.class.getClassLoader());
+	}
+
+	@POST
+	@Path("/")
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response tasksVariables(@Context HttpHeaders headers, SearchPayload payload) {
+
+		searchRequest = new VariablesQueryRequest(payload, PRINT_VERBOSE);
+		searchService = new VariablesQueryService(emf, PRINT_VERBOSE);
+
+		logger.info("VariablesQuery.tasksVariables()");
+		Variant v = getVariant(headers);
+		String contentType = getContentType(headers);
+
+		MarshallingFormat format = MarshallingFormat.fromType(contentType);
+		if (format == null) {
+			format = MarshallingFormat.valueOf(contentType);
+		}
+
+		try {
+
+			searchRequest.printSearchCriteria();
+			searchRequest.filterTaskVariables(SQLConstants.TASK_VAR_PREFIX);
+			searchRequest.filterProcessVariables(SQLConstants.TASK_VAR_PREFIX);
+			searchRequest.filterAttributes();
+			searchService.setRequest(searchRequest);
+
+			searchService.filterByPotentialOwner();
+			searchService.filterByProcessVars();
+			searchService.filterByTasksVars();
+
+			if (searchService.getHaveResults()) {
+
+				Set<IDWrapper> intersect = searchService.intersectResults();
+				searchService.fetchTaskVariables(intersect);
+				searchService.fetchProcessVariables(intersect);
+
+				List<BPMTask> result = searchService.generateResult(intersect);
+
+				String marshall = this.marshaller.marshall(result);
+
+				return createResponse(marshall, v, Response.Status.OK);
+			} else {
+				return createResponse(EMPTY_RESULT, v, Response.Status.OK);
+
+			}
+		} catch (Exception e) {
+			// in case marshalling failed return the call container response to
+			// keep backward compatibility
+			e.printStackTrace();
+			String response = "Execution failed with error : " + e.getMessage();
+			logger.error("Returning Failure response with content '{}'", response);
+			return createResponse(response, v, Response.Status.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
