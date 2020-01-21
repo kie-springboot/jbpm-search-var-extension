d72da2150ab516df5f7d5baee9304fb7da1e003a
Anton Giertli <you@example.com>
2020-01-21 11:51:18 +0000

    inject queue name

--- tests/src/main/java/org/kie/server/springboot/samples/listener/JMSAuditProcessEventListener.java
+++ tests/src/main/java/org/kie/server/springboot/samples/listener/JMSAuditProcessEventListener.java
@@ -1,11 +1,14 @@
 package org.kie.server.springboot.samples.listener;
 
 import org.jbpm.workflow.instance.impl.WorkflowProcessInstanceImpl;
+import org.jbpm.workflow.instance.node.HumanTaskNodeInstance;
 import org.kie.api.event.process.DefaultProcessEventListener;
 import org.kie.api.event.process.ProcessNodeLeftEvent;
+import org.kie.api.runtime.process.NodeInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
@@ -18,23 +21,38 @@ public class JMSAuditProcessEventListener extends DefaultProcessEventListener {
 
 	private String varPayload;
 
+	@Value("${kieserver.auditQueueName}")
+	private String queueName;
+
 	@Override
 	public void afterNodeLeft(ProcessNodeLeftEvent event) {
-		varPayload = "";
 
-		logger.info("Sending jms message");
-		jmsTemplate.setDefaultDestinationName("bpmqueue");
+		NodeInstance node = event.getNodeInstance();
 
-		WorkflowProcessInstanceImpl wpi = (WorkflowProcessInstanceImpl) event.getProcessInstance();
-		wpi.getVariables().keySet().forEach(k -> {
+		if (node instanceof HumanTaskNodeInstance) {
 
-			String tmp = k + "=" + wpi.getVariables().get(k) + ",";
+			logger.info("Sending JMS Message");
 
-			varPayload += tmp;
-		});
+			varPayload = "";
 
-		jmsTemplate.convertAndSend("id:" + event.getProcessInstance().getProcessId() + " ,instance:"
-				+ event.getProcessInstance().getId() + ", variables:\n" + varPayload);
+			jmsTemplate.setDefaultDestinationName("bpmqueue");
+
+			WorkflowProcessInstanceImpl wpi = (WorkflowProcessInstanceImpl) event.getProcessInstance();
+			wpi.getVariables().keySet().forEach(k -> {
+
+				String tmp = k + "=" + wpi.getVariables().get(k) + ",";
+
+				varPayload += tmp;
+			});
+
+			jmsTemplate.convertAndSend("id:" + event.getProcessInstance().getProcessId() + " ,instance:"
+					+ event.getProcessInstance().getId() + ", variables:\n" + varPayload);
+
+		} else {
+
+			logger.info("Skipping JMS audit because node isn't a human task");
+		}
+
 	}
 
 }
